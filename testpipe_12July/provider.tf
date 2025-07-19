128c20fcf3542777e9f563da183534ca46e6975a
lavlesh jha <lavleshjha1001@gmail.com>
2025-07-19 12:56:37 +0530

    backend code removed

--- testpipe_12July/provider.tf
+++ testpipe_12July/provider.tf
@@ -5,15 +5,6 @@ terraform {
       version = "4.36.0"
     }
   }
-
-  backend "azurerm" {
-    subscription_id      = "def6c89e-a855-4b2a-a2d3-a1bc15337302"
-    resource_group_name  = "rg-classic"
-    storage_account_name = "stgclassicpipe"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-
-  }
 }
 
 provider "azurerm" {
